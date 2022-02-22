296337f6a887c3705433e90ab0ff77b7068ac813
Robin Henao <robinson.henao@udea.edu.co>
2022-02-22 16:43:35 -0500

    add script radioButton for ischecket one and fix error of contact data

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20212/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20212/lab1/PersonalDataActivity.kt
@@ -3,7 +3,7 @@ package co.edu.udea.compumovil.gr06_20212.lab1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
-import java.util.*
+import android.widget.RadioButton
 
 
 class PersonalDataActivity : AppCompatActivity() {
@@ -12,8 +12,15 @@ class PersonalDataActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
 
+
         findViewById<EditText>(R.id.etDate).setOnClickListener(){
             showDatePikerDialog()
+        }
+        findViewById<RadioButton>(R.id.radioButtonM).setOnClickListener(){
+            radioButtonCheckM()
+        }
+        findViewById<RadioButton>(R.id.radioButtonF).setOnClickListener(){
+            radioButtonCheckF()
         }
 
     }
@@ -22,8 +29,14 @@ class PersonalDataActivity : AppCompatActivity() {
         val datePicker=DatePickerFragment({day,month,year-> onDateSelected(year,month, day)})
         datePicker.show(supportFragmentManager,"datePiker")
     }
-    fun onDateSelected(day:Int,month:Int,year: Int){
+    private fun onDateSelected(day:Int,month:Int,year: Int){
         findViewById<EditText>(R.id.etDate).setText("$day/$month/$year")
     }
 
+    private fun radioButtonCheckM(){
+        findViewById<RadioButton>(R.id.radioButtonF).isChecked=false
+    }
+    private fun radioButtonCheckF(){
+        findViewById<RadioButton>(R.id.radioButtonM).isChecked=false
+    }
 }
