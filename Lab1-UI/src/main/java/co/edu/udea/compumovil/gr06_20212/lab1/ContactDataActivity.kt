dd106f5e06bc60916d5b10e4a55caae1a9e724ac
John3814 <67084916+John3814@users.noreply.github.com>
2022-02-21 20:38:19 -0500

    "View ContactDataActivity"

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20212/lab1/ContactDataActivity.kt
@@ -0,0 +1,11 @@
+package co.edu.udea.compumovil.gr06_20212.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_data)
+    }
+}
