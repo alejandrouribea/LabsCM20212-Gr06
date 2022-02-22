bdb882fb0f0421e4d3c1b254c9b6bd25fc4a20d0
Robin Henao <robinson.henao@udea.edu.co>
2022-02-21 23:02:04 -0500

    add Calendar funtion in editText and create class for Calendar

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20212/lab1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20212/lab1/MainActivity.kt
@@ -3,9 +3,13 @@ package co.edu.udea.compumovil.gr06_20212.lab1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 }
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20212/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20212/lab1/PersonalDataActivity.kt
@@ -2,10 +2,28 @@ package co.edu.udea.compumovil.gr06_20212.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import java.util.*
+
 
 class PersonalDataActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
+
+        findViewById<EditText>(R.id.etDate).setOnClickListener(){
+            showDatePikerDialog()
+        }
+
     }
+
+    private fun showDatePikerDialog() {
+        val datePicker=DatePickerFragment({day,month,year-> onDateSelected(year,month, day)})
+        datePicker.show(supportFragmentManager,"datePiker")
+    }
+    fun onDateSelected(day:Int,month:Int,year: Int){
+        findViewById<EditText>(R.id.etDate).setText("$day/$month/$year")
+    }
+
 }
